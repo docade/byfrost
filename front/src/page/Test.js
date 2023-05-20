845d06c47597b3fd2830e61a06e0d4e10182d3a4
seungJackie <qkrtmdwo28@gmail.com>
2023-05-20 15:08:39 +0900

    스크롤 1번에 페이지 1회 전환

--- front/src/page/Test.js
+++ front/src/page/Test.js
@@ -1,8 +1,14 @@
 import React from "react";
 import BasicLayout from "../component/LayOut/BasicLayout";
+import FulPage from "../component/FulPgae";
+import FirstContainer from "../component/FirstContainerLayOut/FirstContainerLayOut";
 
 const Test = () => {
-  return <div></div>;
+  return (
+    <FulPage>
+      <FirstContainer />
+    </FulPage>
+  );
 };
 
 export default Test;
